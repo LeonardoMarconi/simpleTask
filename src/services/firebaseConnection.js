aa5ab172cdccf8839ba9ca4f58af84838426b3a0
Leonardo Marconi <leonardo.marconi.pf@hotmail.com>
2022-11-20 20:52:06 -0300

    update keys firebase

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -3,14 +3,14 @@ import 'firebase/database';
 import 'firebase/auth';
 
 let firebaseConfig = {
-    apiKey: "AIzaSyCGybfZPA2_ch6hUiaOInohIw0V3lh4AjM",
-    authDomain: "meuapp-269b1.firebaseapp.com",
-    databaseURL: "https://meuapp-269b1-default-rtdb.firebaseio.com",
-    projectId: "meuapp-269b1",
-    storageBucket: "meuapp-269b1.appspot.com",
-    messagingSenderId: "275442779152",
-    appId: "1:275442779152:web:f91c79f32931ce3f59f695",
-    measurementId: "G-4CWM567TCR"
+    apiKey: "..............................",
+    authDomain: "..............................",
+    databaseURL: "..............................",
+    projectId: "..............................",
+    storageBucket: "..............................",
+    messagingSenderId: "..............................",
+    appId: "..............................",
+    measurementId: ".............................."
 };
 
 if(!firebase.apps.length){
